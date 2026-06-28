4fc21f5c8b203b70b24281f388642e9d9f43af57
Yuuki-Tab <282610875+Yuuki-Tab@users.noreply.github.com>
2026-06-28 20:09:48 +0200

    fix: add allow-top-navigation-by-user-activation to iframe sandbox

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -26,7 +26,7 @@ function VideoEmbed({ project }: { project: Project }) {
 				<iframe
 					src={`https://streamable.com/e/${videoId}?autoplay=1`}
 					allow="autoplay; fullscreen; picture-in-picture"
-					sandbox="allow-scripts allow-same-origin allow-presentation allow-pointer-lock"
+					sandbox="allow-scripts allow-same-origin allow-presentation allow-pointer-lock allow-top-navigation-by-user-activation"
 					title={project.title}
 					style={{ border: "none" }}
 				/>
